b97bbe2f0a1fcfdd862b60532c653018dbe9bc5f
Raphaël Charre <rcharre@oceaneconsulting.com>
2018-12-14 00:07:56 +0100

    Add navbar

--- deusexsoftware/src/main.js
+++ deusexsoftware/src/main.js
@@ -1,20 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 
+import router from './router'
+
+//components
+import App from './App'
+
+//Style
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
+const app = new Vue({
+  render: h => h(App),
   router,
-  components: { App },
-  template: '<App/>'
-})
+  components: {
+  }
+}).$mount('#app')
---
+++ deusexsoftware/src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav-bar></nav-bar>
+    </div>
+    
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
