db80d2d275b0b2825ee179c8eef3c8a533546ece
Téofilo <teofilo.jeandot@gmail.com>
2018-12-21 00:10:52 +0100

    1-tj-20-12-18

--- deusexsoftware/src/pages/ServicesPage.vue
+++ deusexsoftware/src/pages/ServicesPage.vue
@@ -7,32 +7,73 @@
         -->
         <div class="service-banner row  fixed-top">
             <div class="offset-sm-3 col col-sm-2 text-center">
-                <div class="col-xs-12">
-                    <i class="fas fa-code fa-4x d-none d-sm-block"></i>
-                    <i class="fas fa-code fa-2x d-sm-none"></i>
-                </div>
-                <div class="col-xs-12">
-                    <span>Création</span>
-                </div>
+                <a class="service-anchor btn btn-light" href="#creation">
+                    <div class=" col-xs-12">
+                        <i class="fas fa-code fa-3x d-none d-sm-block"></i>
+                        <i class="fas fa-code fa-2x d-sm-none"></i>
+                    </div>
+                    <div class="col-xs-12">
+                        <span class="service-text">Création</span>
+                    </div>
+                </a>
             </div>
             <div class="col col-sm-2 text-center">
-                <div class="col-xs-12">
-                    <i class="fas fa-tools fa-4x d-none d-sm-block"></i>
-                    <i class="fas fa-tools fa-2x d-sm-none"></i>
-                </div>
-                <div class="col-xs-12">
-                    <span>Maintenance</span>
-                </div>
+                <a class="service-anchor btn btn-light" href="#maintenance">
+                    <div class="col-xs-12">
+                        <i class="fas fa-tools fa-3x d-none d-sm-block"></i>
+                        <i class="fas fa-tools fa-2x d-sm-none"></i>
+                    </div>
+                    <div class="col-xs-12">
+                        <span>Maintenance</span>
+                    </div>
+                </a>
             </div>
             <div class="col col-sm-2 text-center">
-                <div class="col-xs-12">
-                    <i class="fas fa-shield-alt fa-4x d-none d-sm-block"></i>
-                    <i class="fas fa-shield-alt fa-2x d-sm-none"></i>
-                </div>
-                <div class="col-xs-12">
-                    <span>Sécurité</span>
-                </div>
+                <a class="service-anchor btn btn-light" href="#security">
+                    <div class="col-xs-12">
+                        <i class="fas fa-shield-alt fa-3x d-none d-sm-block"></i>
+                        <i class="fas fa-shield-alt fa-2x d-sm-none"></i>
+                    </div>
+                    <div class="col-xs-12">
+                        <span>Sécurité</span>
+                    </div>
+                </a>                
             </div>
+        </div>
+        <!-- Main Content -->
+        <div id="creation"></div>
+
+        <div >
+                <h1  class="my-2 page-title">Création</h1>
+           
+            <div class="col-xs-12 col-sm-3">
+
+            </div>
+            <div class="col-xs-12 col-sm-9">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a dignissim augue. Nulla eu metus ut nibh efficitur cursus sit amet non ex. Nunc porta placerat ante, ac efficitur odio interdum condimentum. Mauris vulputate nulla sit amet lacus laoreet, in efficitur libero consequat. Donec faucibus venenatis suscipit. Nunc nunc mi, condimentum in placerat eget, ultricies at ipsum. Curabitur fringilla odio diam, at consequat mauris venenatis eget. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sem libero, rutrum ut feugiat eu, facilisis a odio. Etiam ullamcorper venenatis dui, quis condimentum sem feugiat vitae. Morbi eu nunc lacus. Cras interdum dui sed nisi laoreet varius.
+
+Suspendisse elit neque, rhoncus dictum commodo ornare, viverra in libero. Ut sem diam, dictum eu nulla interdum, aliquet malesuada quam. Vivamus vel odio vitae dolor laoreet luctus eu quis neque. In et nunc arcu. Praesent tempus mauris ac augue aliquam, et dignissim sapien pretium. Nam fermentum nunc quis nisi fermentum sagittis. Nullam facilisis commodo fermentum. Pellentesque mauris nisl, lacinia in luctus non, maximus vitae ante. Nunc quis diam sit amet ipsum lacinia semper at nec diam. Aliquam id hendrerit libero, vel luctus est. 
+            </div>
+        </div>
+        <div id="maintenance"></div>
+        <div >
+            <h1  class="my-2 page-title">Maintenance</h1>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a dignissim augue. Nulla eu metus ut nibh efficitur cursus sit amet non ex. Nunc porta placerat ante, ac efficitur odio interdum condimentum. Mauris vulputate nulla sit amet lacus laoreet, in efficitur libero consequat. Donec faucibus venenatis suscipit. Nunc nunc mi, condimentum in placerat eget, ultricies at ipsum. Curabitur fringilla odio diam, at consequat mauris venenatis eget. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sem libero, rutrum ut feugiat eu, facilisis a odio. Etiam ullamcorper venenatis dui, quis condimentum sem feugiat vitae. Morbi eu nunc lacus. Cras interdum dui sed nisi laoreet varius.
+
+Suspendisse elit neque, rhoncus dictum commodo ornare, viverra in libero. Ut sem diam, dictum eu nulla interdum, aliquet malesuada quam. Vivamus vel odio vitae dolor laoreet luctus eu quis neque. In et nunc arcu. Praesent tempus mauris ac augue aliquam, et dignissim sapien pretium. Nam fermentum nunc quis nisi fermentum sagittis. Nullam facilisis commodo fermentum. Pellentesque mauris nisl, lacinia in luctus non, maximus vitae ante. Nunc quis diam sit amet ipsum lacinia semper at nec diam. Aliquam id hendrerit libero, vel luctus est. 
+        </div>
+        <div id="security"></div>
+        <div>
+            <h1  class="my-2 page-title">Sécurité</h1>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a dignissim augue. Nulla eu metus ut nibh efficitur cursus sit amet non ex. Nunc porta placerat ante, ac efficitur odio interdum condimentum. Mauris vulputate nulla sit amet lacus laoreet, in efficitur libero consequat. Donec faucibus venenatis suscipit. Nunc nunc mi, condimentum in placerat eget, ultricies at ipsum. Curabitur fringilla odio diam, at consequat mauris venenatis eget. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sem libero, rutrum ut feugiat eu, facilisis a odio. Etiam ullamcorper venenatis dui, quis condimentum sem feugiat vitae. Morbi eu nunc lacus. Cras interdum dui sed nisi laoreet varius.
+
+Suspendisse elit neque, rhoncus dictum commodo ornare, viverra in libero. Ut sem diam, dictum eu nulla interdum, aliquet malesuada quam. Vivamus vel odio vitae dolor laoreet luctus eu quis neque. In et nunc arcu. Praesent tempus mauris ac augue aliquam, et dignissim sapien pretium. Nam fermentum nunc quis nisi fermentum sagittis. Nullam facilisis commodo fermentum. Pellentesque mauris nisl, lacinia in luctus non, maximus vitae ante. Nunc quis diam sit amet ipsum lacinia semper at nec diam. Aliquam id hendrerit libero, vel luctus est. 
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a dignissim augue. Nulla eu metus ut nibh efficitur cursus sit amet non ex. Nunc porta placerat ante, ac efficitur odio interdum condimentum. Mauris vulputate nulla sit amet lacus laoreet, in efficitur libero consequat. Donec faucibus venenatis suscipit. Nunc nunc mi, condimentum in placerat eget, ultricies at ipsum. Curabitur fringilla odio diam, at consequat mauris venenatis eget. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sem libero, rutrum ut feugiat eu, facilisis a odio. Etiam ullamcorper venenatis dui, quis condimentum sem feugiat vitae. Morbi eu nunc lacus. Cras interdum dui sed nisi laoreet varius.
+
+Suspendisse elit neque, rhoncus dictum commodo ornare, viverra in libero. Ut sem diam, dictum eu nulla interdum, aliquet malesuada quam. Vivamus vel odio vitae dolor laoreet luctus eu quis neque. In et nunc arcu. Praesent tempus mauris ac augue aliquam, et dignissim sapien pretium. Nam fermentum nunc quis nisi fermentum sagittis. Nullam facilisis commodo fermentum. Pellentesque mauris nisl, lacinia in luctus non, maximus vitae ante. Nunc quis diam sit amet ipsum lacinia semper at nec diam. Aliquam id hendrerit libero, vel luctus est. 
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a dignissim augue. Nulla eu metus ut nibh efficitur cursus sit amet non ex. Nunc porta placerat ante, ac efficitur odio interdum condimentum. Mauris vulputate nulla sit amet lacus laoreet, in efficitur libero consequat. Donec faucibus venenatis suscipit. Nunc nunc mi, condimentum in placerat eget, ultricies at ipsum. Curabitur fringilla odio diam, at consequat mauris venenatis eget. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sem libero, rutrum ut feugiat eu, facilisis a odio. Etiam ullamcorper venenatis dui, quis condimentum sem feugiat vitae. Morbi eu nunc lacus. Cras interdum dui sed nisi laoreet varius.
+
+Suspendisse elit neque, rhoncus dictum commodo ornare, viverra in libero. Ut sem diam, dictum eu nulla interdum, aliquet malesuada quam. Vivamus vel odio vitae dolor laoreet luctus eu quis neque. In et nunc arcu. Praesent tempus mauris ac augue aliquam, et dignissim sapien pretium. Nam fermentum nunc quis nisi fermentum sagittis. Nullam facilisis commodo fermentum. Pellentesque mauris nisl, lacinia in luctus non, maximus vitae ante. Nunc quis diam sit amet ipsum lacinia semper at nec diam. Aliquam id hendrerit libero, vel luctus est. 
         </div>
     </div>
 </template>
@@ -47,13 +88,34 @@ export default {
 
 .service-banner{
     background-color: whitesmoke;
-    padding-top: 10px;
-    padding-bottom: 10px;
+    padding-top: 5px;
+    padding-bottom: 5px;
     margin-top:56px;
 }
 
 .padding-page-service{
     padding-top: 56px;
 }
+
+.padding-bloc-service{
+    padding-top: 60px;
+    padding-bottom: 60px;
+}
+
+.service-anchor {
+  background-color: transparent;
+  border:none;  
+}
+
+.service-anchor:hover span{
+    color: #007bff;
+}
+
+#maintenance {
+    padding-top: 120px;
+}
+
+
+
 
 </style>
